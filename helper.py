add8ffea2fe6c21c7a1d9de1b86c73c52bd6fefe
singhp99 <141775356+singhp99@users.noreply.github.com>
2025-05-28 13:36:00 -0400

    Delete helper.py

--- helper.py
+++
@@ -1,6 +0,0 @@
-
-from spyral.phases.pointcloud_legacy_phase import get_event_range
-
-def event_range(trace_file):
-    min_event, max_event = get_event_range(trace_file)
-    return min_event, max_event 
