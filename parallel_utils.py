e45402c10b63688a4615c96883d25afdc606994e
singhp99 <141775356+singhp99@users.noreply.github.com>
2025-05-28 13:25:49 -0400

    Add files via upload

---
+++ parallel_utils.py
@@ -0,0 +1,19 @@
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from tqdm.notebook import tqdm
+from event_processing import process_event
+import sys
+
+def parallelize(results,run_number,min_event, max_event, trace_file_path):
+    # results = []
+    with ProcessPoolExecutor() as executor:
+        futures = {executor.submit(process_event, ev, trace_file_path): ev for ev in range(min_event, max_event)}
+
+        for future in tqdm(as_completed(futures), total=len(futures), desc="Processing Events", file=sys.stdout):
+            res = future.result()
+            if res is not None:
+                event_number,drift,micromg,window = res
+
+                row_data = {"run_number":run_number,"event_number":event_number,"drift_velocity_tb":drift,"micromegas_tb":micromg,"window_tb":window}
+                results.append(row_data)
+
+    return results
