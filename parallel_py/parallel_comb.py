cf0589e96635696ce64346a1b3a95d68fda5f8da
Pranjal Singh <pranjalsingh31299@gmail.com>
2025-06-02 10:56:08 -0400

    Routine update

--- parallel_py/parallel_comb.py
+++ parallel_py/parallel_comb.py
@@ -1,6 +1,7 @@
 from spyral.core.run_stacks import form_run_string
 from pathlib import Path
 import h5py as h5
+import os
 
 from event_processing import process_event
 from parallel_utils import parallelize
@@ -11,7 +12,8 @@ import pandas as pd
 def main():
     trace_path = Path("/mnt/scratch/singhp19/O16_runs")
     all_valid_events = []
-    run_num_ls = [104,105,106,108,109,110,111,112,113,114,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,169]
+    run_num_ls = [53,54,55,56,58,59,60,61,62,63,64,65,66,67,68,70,71,72,73,74,77,78,83,84,85,86,87,88,89,90,91,92,94,96,97,98,99,100,101,102,103,104,105,106,108,109,110,111,112,113,114,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,139,140,141,142,143,144,145,146,148,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169]
+
     for run_number in run_num_ls:
         trace_file_path = trace_path / f"{form_run_string(run_number)}.h5"
 
